a8470d90260c22c92cf0045c56eaeb1abba13c78
Pauline Holm <72187309+paulineholm@users.noreply.github.com>
2020-12-01 00:54:48 +0100

    Add files via upload

--- dm-footer-common.js
+++ dm-footer-common.js
@@ -2,9 +2,9 @@ let dmfooter = document.querySelector(".dm-footer");
 
 function dmGenerateFooter(){
     let footer = document.querySelector(".footer");
-    let dmHTML = '<p>2020 © Handcrafted with love by Pauline Holm |<a href="mailto:paulinabryzgiel@gmail.com"> paulinabryzgiel@gmail.com</a><br>'
+    let dmHTML = '<p>2020 © Handcrafted with love by Pauline Holm |<a href="mailto:frk.paulineholm@gmail.com"> frk.paulineholm@gmail.com</a><br>'
 
-    dmHTML += '<a href="https://www.facebook.com/pauline.holm.pau/"> fb </a>|<a href="https://instagram.com/frk.holminka/"> ig </a>|<a href="https:/www.linkedin.com/in/paulineholm/"> in </a>|<a href="https:/github.com/paulineholm/"> gh </a>';
+    dmHTML += '<a href="https://www.facebook.com/pauline.holm.pau/"> fb </a>|<a href="https://instagram.com/frk.holminka/"> ig </a>|<a href="https://linkedin.com/in/paulineholm/"> in </a>|<a href=" https://github.com/paulineholm/"> gh </a>';
 
     dmHTML += '</p>';
 
